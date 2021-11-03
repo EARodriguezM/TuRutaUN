f67ee71ae288e25671e7855bd31686cb56cf5c5e
EARodriguezM <esarodriguezme@gmail.com>
2021-11-03 00:13:07 -0500

    default data in db

---
+++ 01_database_scripts/insert_data.sql
@@ -0,0 +1,7 @@
+---------------------------------------------------------------
+USE [TuRutaUN]
+GO
+INSERT INTO user_type (user_type_id, description, status) VALUES (1, 'Administrador', 1);
+INSERT INTO user_type (user_type_id, description, status) VALUES (2, 'Conductor', 1);
+INSERT INTO user_type (user_type_id, description, status) VALUES (3, 'Usuario', 1);
+GO
